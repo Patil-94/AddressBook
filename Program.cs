f62d4fa952abff408e28d31f0b88b87c3860eb79
Patil-94 <ankitakhairnar10@.com>
2021-04-18 21:55:17 +0530

    [Ankita] Add:create contact in address book

--- Program.cs
+++ Program.cs
@@ -8,10 +8,25 @@ namespace AddressBook
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program ");
+            Console.WriteLine("-------------Welcome to Address Book Program------------ ");
+            Person  person  = new Person("Ankita","patil","dehu","pune","maharashtra","412109","9146852365","ankitakhairnar00@gmail.com");
+            Console.WriteLine( person .getName());
+            Console.WriteLine(person.getlastName());
+            Console.WriteLine(person.getaddress());
+            Console.WriteLine(person.getcity());
+            Console.WriteLine(person.getstate());
+            Console.WriteLine(person.getzip());
+            Console.WriteLine(person.getphone());
+            Console.WriteLine(person.getemail());
+            //contacts.FirstName = anki
             Console.Read();
         }
+        
+        
+
     }
 }
