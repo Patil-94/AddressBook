bc1da9be1708ec106e9342f32f9f3b4a81bc043f
Patil-94 <ankitakhairnar10@.com>
2021-04-19 13:06:13 +0530

    [Ankita] Add:create New contact in address book

---
+++ IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+     interface IPerson
+    {
+         void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,16 +13,25 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("-------------Welcome to Address Book Program------------ ");
-            Person  person  = new Person("Ankita","patil","dehu","pune","maharashtra","412109","9146852365","ankitakhairnar00@gmail.com");
-            Console.WriteLine( person .getName());
-            Console.WriteLine(person.getlastName());
-            Console.WriteLine(person.getaddress());
-            Console.WriteLine(person.getcity());
-            Console.WriteLine(person.getstate());
-            Console.WriteLine(person.getzip());
-            Console.WriteLine(person.getphone());
-            Console.WriteLine(person.getemail());
-            //contacts.FirstName = anki
+            AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Enter First Name :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+            addressBook.ViewContact();
             Console.Read();
         }
         
